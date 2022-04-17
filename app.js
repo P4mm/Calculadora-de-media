5db408f5229fa88901db860bd8f9e7130113bd6c
P4mm <100779250+P4mm@users.noreply.github.com>
2022-04-17 19:02:28 -0300

    Primeiros passos - Front End

---
+++ app.js
@@ -0,0 +1,17 @@
+var nome = "Pamela"
+
+var NotaDoPrimeiroBimestre = 9
+var NotaDoSegundoBimestre = 7
+var NotaDoTerceiroBimestre = 4
+var NotaDoQuartoBimestre = 2
+
+var NotaFinal = (NotaDoPrimeiroBimestre + NotaDoSegundoBimestre + NotaDoTerceiroBimestre + NotaDoQuartoBimestre) / 4
+
+var NotaFixada = NotaFinal.toFixed(1)
+
+console.log("Bem vinda " + nome)
+console.log(NotaFixada)
+
+// isso é um comentário
+
+// Revisão: Variável, string, console.log, toFixed, operações matemáticas, concatenação, 
